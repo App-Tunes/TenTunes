f5d5c574106b81c8d9e786aa49b3cda03358d506
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-02-17 17:56:17 +0100

    Initial Version 

--- TenTunes/ViewController.swift
+++ TenTunes/ViewController.swift
@@ -8,7 +8,28 @@
 
 import Cocoa
 
+var database = [
+    [
+        "artist": "Air",
+        "title": "Caramel Prisoner",
+        "album": "10,000 Hz Legend",
+    ],
+    [
+        "artist": "Air",
+        "title": "Don't Be Light",
+        "album": "10,000 Hz Legend",
+        ],
+    [
+        "artist": "Boris Blank",
+        "title": "Midnight Procession",
+        "album": "Electrified",
+        ],
+
+]
+
 class ViewController: NSViewController {
+
+    @IBOutlet var tableView: NSTableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,3 +46,45 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSTableViewDelegate {
+    
+    fileprivate enum CellIdentifiers {
+        static let NameCell = NSUserInterfaceItemIdentifier(rawValue: "nameCell")
+        static let DateCell = NSUserInterfaceItemIdentifier(rawValue: "dateCell")
+        static let SizeCell = NSUserInterfaceItemIdentifier(rawValue: "sizeCell")
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .long
+        
+        // 1
+        let item = database[row]
+        
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            if let cell = tableView.makeView(withIdentifier: CellIdentifiers.NameCell, owner: nil) as? NSTableCellView {
+                let artist = item["artist"]!
+                let title = item["title"]!
+                let album = item["album"]!
+
+                cell.textField?.stringValue = "\(artist) - \(title) (\(album))"
+                return cell
+            }
+        } else if tableColumn == tableView.tableColumns[1] {
+            
+        } else if tableColumn == tableView.tableColumns[2] {
+            
+        }
+        
+        return nil
+    }
+}
+
+extension ViewController: NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return database.count;
+    }
+}

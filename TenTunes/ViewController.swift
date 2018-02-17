155e1c6d6551355ddae670cf83f924e1a7c182d3
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-02-17 16:23:50 +0100

    Initial Commit

---
+++ TenTunes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TenTunes
+//
+//  Created by Lukas Tenbrink on 17.02.18.
+//  Copyright © 2018 ivorius. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

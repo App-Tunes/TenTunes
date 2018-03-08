30300e722ccf0185082613b4a6018026fa81cdad
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-03-08 12:56:29 +0100

    Add M3U auto export.

--- TenTunes/Cocoa+TT.swift
+++ TenTunes/Cocoa+TT.swift
@@ -411,6 +411,11 @@ extension String {
             return String(self[startIndex...endIndex])
         }
     }
+    
+    var asFileName: String {
+        let escaped = replacingOccurrences(of: "/", with: "\\:\\") // Escape
+        return escaped.components(separatedBy: ":").joined(separator: "_") // Remove :
+    }
 }
 
 extension NSManagedObject {
@@ -434,5 +439,30 @@ extension NSMenu {
 extension DispatchSemaphore {
     func acquireNow() -> Bool {
         return wait(timeout: DispatchTime.now()) == .success
+    }
+}
+
+extension URL {
+    func relativePath(from base: URL) -> String? {
+        // Ensure that both URLs represent files:
+        guard self.isFileURL && base.isFileURL else {
+            return nil
+        }
+        
+        // Remove/replace "." and "..", make paths absolute:
+        let destComponents = self.standardized.pathComponents
+        let baseComponents = base.standardized.pathComponents
+        
+        // Find number of common path components:
+        var i = 0
+        while i < destComponents.count && i < baseComponents.count
+            && destComponents[i] == baseComponents[i] {
+                i += 1
+        }
+        
+        // Build relative path:
+        var relComponents = Array(repeating: "..", count: baseComponents.count - i)
+        relComponents.append(contentsOf: destComponents[i...])
+        return relComponents.joined(separator: "/")
     }
 }
--- TenTunes/Music/Library+Export.swift
+++ TenTunes/Music/Library+Export.swift
@@ -22,14 +22,44 @@ extension Library {
         return true
     }
     
+    func exportDirectory(title: String) -> URL {
+        let exportsDirectory = directory.appendingPathComponent("Exports", isDirectory: true)
+        let url = exportsDirectory.appendingPathComponent(title, isDirectory: true)
+        try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+        return url
+    }
+    
     func updateExports(in mox: NSManagedObjectContext) {
-        let tracks = try! mox.fetch(Track.fetchRequest())
+        let tracks: [Track] = try! mox.fetch(Track.fetchRequest())
         // TODO Sort playlist by their parent / child tree
-        let playlists = try! mox.fetch(Playlist.fetchRequest())
+        let playlists: [Playlist] = try! mox.fetch(Playlist.fetchRequest())
+        
+        let m3uRelative = exportDirectory(title: "M3U (Relative)")
+        let m3uAbsolute = exportDirectory(title: "M3U (Absolute)")
+        
+        for playlist in playlists {
+            Library.writeM3U(playlist: playlist, to: m3uRelative.appendingPathComponent(playlist.name.asFileName + ".m3u", isDirectory: false), absolute: false)
+            Library.writeM3U(playlist: playlist, to: m3uAbsolute.appendingPathComponent(playlist.name.asFileName + ".m3u", isDirectory: false), absolute: true)
+        }
         
         _exportsRequireUpdate = false
         // Set this after fetching so no changes remain unexported
         
         exportSemaphore.signal()
+    }
+    
+    static func writeM3U(playlist: Playlist, to: URL, absolute: Bool) {
+        let tracks: [String] = playlist.tracksList.map { track in
+            // TODO Duration
+            let info = "#EXTINF:0,\(track.rAuthor) - \(track.rTitle)"
+            
+            // TODO Put in path whether it exists or not
+            let url = track.url ?? URL(fileURLWithPath: "unknown")
+            
+            return info + "\n" + (absolute ? url.path : (url.relativePath(from: to) ?? url.path))
+        }
+        let contents = "#EXTM3U\n" + tracks.joined(separator: "\n")
+        
+        try! contents.write(to: to, atomically: true, encoding: .utf8)
     }
 }

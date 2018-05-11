0a63d63092369aa5ae0c8d32fe6b1932b95e5171
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-05-11 21:19:06 +0200

    Move PlaylistController's and TrackController's pasteboard content to their own files.

--- TenTunes/View/PlaylistController/PlaylistController.swift
+++ TenTunes/View/PlaylistController/PlaylistController.swift
@@ -185,66 +185,6 @@ extension PlaylistController : NSOutlineViewDelegate {
     func outlineView(_ outlineView: NSOutlineView, rowViewForItem item: Any) -> NSTableRowView? {
         return VibrantTableRowView()
     }
-    
-    // Pasteboard, Dragging
-    
-    func outlineView(_ outlineView: NSOutlineView, pasteboardWriterForItem item: Any) -> NSPasteboardWriting? {
-        let pbitem = NSPasteboardItem()
-        Library.shared.writePlaylist(item as! Playlist, toPasteboarditem: pbitem)
-        return pbitem
-    }
-
-    func outlineView(_ outlineView: NSOutlineView, validateDrop info: NSDraggingInfo, proposedItem item: Any?, proposedChildIndex index: Int) -> NSDragOperation {
-        guard let type = info.draggingPasteboard().availableType(from: outlineView.registeredDraggedTypes) else {
-            return []
-        }
-        
-        switch type {
-        case Track.pasteboardType:
-            let playlist = item as? Playlist ?? masterPlaylist!
-            return Library.shared.isEditable(playlist: playlist) ? .move : []
-        case Playlist.pasteboardType:
-            // We can always rearrange, except into playlists
-            let playlist = item as? Playlist ?? masterPlaylist
-            if !(playlist is PlaylistFolder) {
-                return []
-            }
-            return .move
-        default:
-            fatalError("Unhandled, but registered pasteboard type")
-        }
-    }
-    
-    func outlineView(_ outlineView: NSOutlineView, acceptDrop info: NSDraggingInfo, item: Any?, childIndex index: Int) -> Bool {
-        let pasteboard = info.draggingPasteboard()
-        
-        guard let type = pasteboard.availableType(from: outlineView.registeredDraggedTypes) else {
-            return false
-        }
-        
-        let parent = item as? Playlist ?? masterPlaylist!
-
-        switch type {
-        case Track.pasteboardType:
-            let tracks = (pasteboard.pasteboardItems ?? []).compactMap(Library.shared.readTrack)
-
-            (parent as! PlaylistManual).addTracks(tracks)
-            return true
-        case Playlist.pasteboardType:
-            let playlists = (pasteboard.pasteboardItems ?? []).compactMap(Library.shared.readPlaylist)
-            
-            for playlist in playlists {
-                (parent as! PlaylistFolder).addPlaylist(playlist, above: index >= 0 ? index : nil)
-            }
-            break
-        default:
-            fatalError("Unhandled, but registered pasteboard type")
-        }
-        
-        try! Library.shared.viewContext.save()
-        
-        return true
-    }
 }
 
 extension PlaylistController: NSTextFieldDelegate {
---
+++ TenTunes/View/TrackController/TrackController+Pasteboard.swift
@@ -0,0 +1,86 @@
+//
+//  TrackController+Pasteboard.swift
+//  TenTunes
+//
+//  Created by Lukas Tenbrink on 11.05.18.
+//  Copyright © 2018 ivorius. All rights reserved.
+//
+
+import Foundation
+
+import AVFoundation
+
+extension TrackController {
+    func tableView(_ tableView: NSTableView, pasteboardWriterForRow row: Int) -> NSPasteboardWriting? {
+        let item = NSPasteboardItem()
+        Library.shared.writeTrack(history.track(at: row)!, toPasteboarditem: item)
+        return item
+    }
+    
+    func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableView.DropOperation) -> NSDragOperation {
+        guard !isQueue else {
+            return dropOperation == .above ? .move : []
+        }
+        
+        if dropOperation == .above, Library.shared.isEditable(playlist: history.playlist), history.isUnsorted {
+            return .move
+        }
+        return []
+    }
+    
+    func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableView.DropOperation) -> Bool {
+        let pasteboard = info.draggingPasteboard()
+        let tracks = (pasteboard.pasteboardItems ?? []).compactMap(Library.shared.readTrack)
+        
+        if isQueue {
+            let tracksBefore = history.tracks
+            
+            if (info.draggingSource() as AnyObject) === _tableView {
+                history.rearrange(tracks: tracks, before: row)
+            }
+            else {
+                history.insert(tracks: tracks, before: row)
+            }
+            
+            _tableView.animateDifference(from: tracksBefore, to: history.tracks)
+        }
+        else {
+            (history.playlist as! PlaylistManual).addTracks(tracks, above: row)
+            try! Library.shared.viewContext.save()
+        }
+        
+        return true
+    }
+    
+    func tableView(_ tableView: NSTableView, sortDescriptorsDidChange oldDescriptors: [NSSortDescriptor]) {
+        // TODO We only care about the first
+        if let descriptor = tableView.sortDescriptors.first, let key = descriptor.key, key != "none" {
+            switch key {
+            case "title":
+                desired.sort = { $0.rTitle < $1.rTitle }
+            case "genre":
+                desired.sort = { Optional<String>.compare($0.genre, $1.genre) }
+            case "key":
+                desired.sort = { Optional<Key>.compare($0.key, $1.key) }
+            case "bpm":
+                desired.sort = { ($0.bpm ?? 0) < ($1.bpm ?? 0)  }
+            case "duration":
+                desired.sort = { ($0.duration ?? kCMTimeZero) < ($1.duration ?? kCMTimeZero)  }
+            default:
+                fatalError("Unknown Sort Descriptor Key")
+            }
+            
+            // Hax
+            if !descriptor.ascending {
+                let sorter = desired.sort!
+                desired.sort = { !sorter($0, $1) }
+            }
+        }
+        else {
+            desired.sort = nil
+            tableView.sortDescriptors = [] // Update the view so it doesn't show an arrow on the affected columns
+        }
+        
+        desired._changed = true
+    }
+}

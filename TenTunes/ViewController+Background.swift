a55d37e653ad8420fe6c1b0e989301f911166553
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-07-22 20:37:26 +0200

    Do a regular player sanity check 10 times a second.

--- TenTunes/ViewController+Background.swift
+++ TenTunes/ViewController+Background.swift
@@ -32,6 +32,8 @@ extension ViewController {
         self.backgroundTimer = Timer.scheduledTimer(withTimeInterval: 1.0 / 10.0, repeats: true ) { [unowned self] (timer) in
             Library.shared.considerExport()
             Library.shared.considerSanity()
+            
+            self.player.sanityCheck()
 
             var taskers = PriorityQueue(ascending: true, startingValues: self.taskers)
             taskers.push(self.tasker)

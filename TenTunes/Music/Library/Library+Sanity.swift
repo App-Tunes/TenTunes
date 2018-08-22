9d76b2b680411f5a3e5f58a9936a82fcee091d0a
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-08-22 10:16:48 +0100

    Fix sanity not being checked due to the semaphore never being released.

--- TenTunes/Music/Library/Library+Sanity.swift
+++ TenTunes/Music/Library/Library+Sanity.swift
@@ -64,7 +64,7 @@ extension Library {
     }
 
     func considerSanity() {
-        guard sanitySemaphore.acquireNow(), sanityChanged else {
+        guard sanityChanged, sanitySemaphore.acquireNow() else {
             return
         }
         

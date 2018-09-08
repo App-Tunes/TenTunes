d27f484a3a5cc3c83a32f0cab464278cf6d71513
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-09-08 02:04:57 +0200

    Improve the fucking visualiizeeer.

--- TenTunes/Shader/visualizer.fs
+++ TenTunes/Shader/visualizer.fs
@@ -47,11 +47,11 @@ void main( void ) {
 
     float pTime = time * 0.1;
     // Time-shift depending on x/y coord for some cool patterns
-    for (int i = 0; i < pointCount; i+=2) {
+    for (int i = 0; i < pointCount; i++) {
         float freqRatio = (float(i / points_per_freq) / freqCount);
         pTime += sin(  centerX * sin(time * (0.0754 + freqRatio * 0.0154) + freqRatio) / ((1 - freqRatio) * 32)
                      + centerY * cos(time * (0.0834 + freqRatio * 0.0146) + freqRatio) / ((1 - freqRatio) * 32))
-        * (0.0041 + (1 - freqRatio) * 0.0053) * frequencies[i / points_per_freq];
+        * (0.0021 + (1 - freqRatio) * 0.0033) * frequencies[i / points_per_freq];
     }
 //    pTime += sin(centerX * sin(time * 0.0754) / 32.0 + centerY * cos(time * 0.0834) / 32.0) * 0.07 * lows;
 //    pTime += sin(centerX * cos(time * 0.1) / 8.0 + centerY * sin(time * 0.11) / 8.0) * 0.01 * mids;

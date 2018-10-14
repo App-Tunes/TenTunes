45992cd1c333b838da144107fc61a60eec74c92a
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-10-14 12:51:47 +0200

    Make things less white.

--- TenTunes/View/Visualizer/VisualizerView.swift
+++ TenTunes/View/Visualizer/VisualizerView.swift
@@ -114,11 +114,12 @@ class VisualizerView: RFOpenGLView {
         let ratio: CGFloat = resonance[idx] / totalResonance
         
         let localDarkness = pow(2, CGFloat((1 - brightness) * (darknessBonus * 2 + 1)) + 0.4)
-        let brightnessBoost = pow(0.5, ((1 - ratio) * 0.5 + 0.1) / (highResonance / 10 + 1)) + ratio * 0.3
+        let brightnessBoost = pow(0.5, ((1 - ratio) * 0.4 + 0.4) / (highResonance / 15 + 1)) + ratio * 0.2
+        let desaturationBoost = (0.5 + prog * 0.5) * totalResonance / 60 + prog * 0.6
 
         // 0.6 so that extremely high and low sounds are far apart in color
         return NSColor(hue: (prog * colorVariance + CGFloat(time * 0.02321)).truncatingRemainder(dividingBy: 1),
-                       saturation: max(0, min(1, ratio * 4 - prog - totalResonance / 35)),
+                       saturation: max(0, min(1, 0.2 + ratio * 4 - desaturationBoost)),
                        brightness: min(1, resonance[idx] * 2 + brightnessBoost * 0.4) / localDarkness + 0.4,
                        alpha: 1)
     }

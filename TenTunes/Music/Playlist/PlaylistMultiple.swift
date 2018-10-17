295f2cf9c6abd5ea0b1d5111385e3d72bfee2c8f
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-10-17 23:04:46 +0200

    Make PlaylistMultiple editable (well, just add and remove).

--- TenTunes/Music/Playlist/PlaylistMultiple.swift
+++ TenTunes/Music/Playlist/PlaylistMultiple.swift
@@ -28,3 +28,34 @@ class PlaylistMultiple : PlaylistProtocol {
         return converted.count == playlists.count ? type(of: self).init(playlists: converted) : nil
     }
 }
+
+extension PlaylistMultiple : ModifiablePlaylist {
+    var modifablePlaylists: [ModifiablePlaylist]? {
+        return playlists as? [ModifiablePlaylist]
+    }
+    
+    func _supports(action: ModifyingAction, rguard: RecursionGuard<Playlist>) -> Bool {
+        // Can only delete, if we want to add then what do we even add to??
+        guard action == .delete else {
+            return false
+        }
+        
+        return self.modifablePlaylists?.allSatisfy { $0.supports(action: action) } ?? false
+    }
+    
+    func confirm(action: ModifyingAction) -> Bool {
+        switch action {
+        case .delete:
+            return NSAlert.confirm(action: "Remove from all playlists", text: "The tracks will be removed from all playlists in this selection (\( modifablePlaylists!.count )).")
+        default:
+            return true
+        }
+    }
+    
+    func removeTracks(_ tracks: [Track]) {
+        for playlist in modifablePlaylists! {
+            playlist.removeTracks(tracks)
+        }
+    }
+}
+

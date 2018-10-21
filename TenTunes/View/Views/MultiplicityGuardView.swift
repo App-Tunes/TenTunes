427b57591a853008023943d858daa06155eac37d
Lukas Tenbrink <lukastenbrink@googlemail.com>
2018-10-21 17:30:13 +0200

    Improve compression handling for multiplicity elements.

--- TenTunes/View/Views/MultiplicityGuardView.swift
+++ TenTunes/View/Views/MultiplicityGuardView.swift
@@ -83,15 +83,22 @@ class MultiplicityGuardView: NSView {
         _manyTextField.drawsBackground = false
         _manyTextField.isBordered = false
         _manyTextField.textColor = .secondaryLabelColor
+        _manyTextField.setContentCompressionResistancePriority(.init(rawValue: 50), for: .horizontal)
         
         _confirmShowMany = NSButton(title: "Show", target: self, action: #selector(showSuggested))
         _confirmShowMany.translatesAutoresizingMaskIntoConstraints = false
         
         _manyPlaceholder!.addSubview(_manyTextField)
         _manyPlaceholder!.addSubview(_confirmShowMany)
+        
+        // Center
         _manyPlaceholder!.addConstraints(NSLayoutConstraint.center(in: _manyPlaceholder!, for: _confirmShowMany!))
         _manyPlaceholder?.addConstraint(NSLayoutConstraint(item: _manyTextField, attribute: .bottom, relatedBy: .equal, toItem: _confirmShowMany, attribute: .top, multiplier: 1, constant: -10))
         _manyPlaceholder?.addConstraint(NSLayoutConstraint(item: _manyTextField, attribute: .centerX, relatedBy: .equal, toItem: _confirmShowMany, attribute: .centerX, multiplier: 1, constant: 0))
+        
+        // Don't squish
+        _manyPlaceholder?.addConstraint(NSLayoutConstraint(item: _manyTextField, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: _manyPlaceholder!, attribute: .leading, multiplier: 1, constant: 0))
+        _manyPlaceholder?.addConstraint(NSLayoutConstraint(item: _confirmShowMany, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: _manyPlaceholder!, attribute: .leading, multiplier: 1, constant: 0))
         
         //
         
@@ -104,9 +111,11 @@ class MultiplicityGuardView: NSView {
         _errorTextField.drawsBackground = false
         _errorTextField.isBordered = false
         _errorTextField.textColor = .secondaryLabelColor
+        _errorTextField.setContentCompressionResistancePriority(.init(rawValue: 50), for: .horizontal)
         
         _errorPlaceholder!.addSubview(_errorTextField)
         _errorPlaceholder!.addConstraints(NSLayoutConstraint.center(in: _errorPlaceholder!, for: _errorTextField!))
+        _errorPlaceholder?.addConstraint(NSLayoutConstraint(item: _errorTextField, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: _errorPlaceholder!, attribute: .leading, multiplier: 1, constant: 0))
         
         //
 

0398b1a798f456d55459c1d2fbf0d33f9ae407ba
Lukas Tenbrink <lukastenbrink@googlemail.com>
2019-01-08 23:18:29 +0100

    Output track loudness in its track editor infos.

--- TenTunes/View/TrackController/Track Editor/TrackEditor.swift
+++ TenTunes/View/TrackController/Track Editor/TrackEditor.swift
@@ -39,6 +39,7 @@ class TrackEditor: NSViewController {
             ]),
         GroupData(title: "Info", icon: #imageLiteral(resourceName: "info"), data: [
             InfoData(title: "Duration") { $0.rDuration },
+            InfoData(title: "Loudness") { String(format: "%0.2f", $0.loudness) },
             InfoData(title: "Kbps") { String(format: "%0.2f", $0.bitrate / 1024) },
             InfoData(title: "Location") { $0.path ?? "" },
             ]),

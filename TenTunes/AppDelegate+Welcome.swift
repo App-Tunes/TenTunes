97b8f3fa9c3f6ceaf026ed2297ebba90a658b9c8
Lukas Tenbrink <lukastenbrink@googlemail.com>
2019-04-28 17:02:54 +0200

    Reorder welcome file.

--- TenTunes/AppDelegate+Welcome.swift
+++ TenTunes/AppDelegate+Welcome.swift
@@ -9,86 +9,6 @@
 import Cocoa
 
 extension AppDelegate {
-    func chooseLibrary(create: Bool) -> Bool {
-        var location: URL?
-        
-        if create {
-            let dialog = NSSavePanel()
-            location = dialog.runModal() == .OK ? dialog.url : nil
-        }
-        else {
-            let dialog = NSOpenPanel()
-            
-            dialog.canChooseFiles = true // packages are considered files by finder, library is a package
-            dialog.canChooseDirectories = true
-            dialog.allowedFileTypes = ["de.ivorius.tentunes.library"]
-            dialog.directoryURL = location
-            
-            location = dialog.runModal() == .OK ? dialog.url : nil
-        }
-        
-        // TODO Allow overrides (the save dialogue specifically prompts for it)
-        return location.map { tryLibrary(at: $0, create: create) } ?? false
-    }
-    
-    func addLibraryWelcomeSteps(workflow: WorkflowWindowController, isFirstLaunch: Bool, force: Bool?) {
-        let isNewLibrary =
-            persistentContainer![PlaylistRole.playlists].children.count == 0 &&
-            persistentContainer![PlaylistRole.library].tracksList.count == 0
-        
-        if force ?? isNewLibrary {
-            workflow.addStep(
-                .interaction(ConfirmStep.create(
-                    text: "Let's get your new library set up.",
-                    buttonText: "Sure"
-                ))
-            )
-            
-            workflow.addSteps([
-                .interaction(OptionsStep.create(text: "So where do we start?", options: [
-                    .create(text: "Import iTunes Library", image: NSImage(named: .iTunesName)!) { [unowned self] in
-                        return self.importFromITunes(flat: true)
-                    },
-                    .create(text: "Import Music Folder", image: NSImage(named: .musicName)!) { [unowned self] in
-                        return self.importFromDirectory()
-                    },
-                    .create(text: "Start Fresh", image: NSImage(named: .nameName)!) {
-                        return true
-                    },
-                ]))
-            ])
-        }
-        
-        // Not guaranteed to be new, but really
-        // If the user has made neither playlists nor imported tracks
-        // They probably want this screen anyway
-        
-        if !workflow.isEmpty {
-            if isFirstLaunch {
-                workflow.addStep(
-                    .interaction(ConfirmStep.create(
-                        text: "And that's it!\nHave fun with Ten Tunes!",
-                        buttonText: "Let's Go!",
-                        mode: .complete
-                    ) { [unowned self] in
-                        self.commenceAfterWelcome()
-                    })
-                )
-            }
-            else {
-                workflow.addStep(
-                    .interaction(ConfirmStep.create(
-                        text: "Alright, that's it. Have fun!",
-                        buttonText: "Cool, thanks!",
-                        mode: .complete
-                    ) { [unowned self] in
-                        self.commenceAfterWelcome()
-                    })
-                )
-            }
-        }
-    }
-    
     func addWelcomeSteps(workflow: WorkflowWindowController, chooseLibrary: Bool) {        
         #if DEBUG_WELCOME
         let force: Bool? = true
@@ -151,6 +71,86 @@ extension AppDelegate {
         }
     }
     
+    func addLibraryWelcomeSteps(workflow: WorkflowWindowController, isFirstLaunch: Bool, force: Bool?) {
+        let isNewLibrary =
+            persistentContainer![PlaylistRole.playlists].children.count == 0 &&
+                persistentContainer![PlaylistRole.library].tracksList.count == 0
+        
+        if force ?? isNewLibrary {
+            workflow.addStep(
+                .interaction(ConfirmStep.create(
+                    text: "Let's get your new library set up.",
+                    buttonText: "Sure"
+                ))
+            )
+            
+            workflow.addSteps([
+                .interaction(OptionsStep.create(text: "So where do we start?", options: [
+                    .create(text: "Import iTunes Library", image: NSImage(named: .iTunesName)!) { [unowned self] in
+                        return self.importFromITunes(flat: true)
+                    },
+                    .create(text: "Import Music Folder", image: NSImage(named: .musicName)!) { [unowned self] in
+                        return self.importFromDirectory()
+                    },
+                    .create(text: "Start Fresh", image: NSImage(named: .nameName)!) {
+                        return true
+                    },
+                    ]))
+                ])
+        }
+        
+        // Not guaranteed to be new, but really
+        // If the user has made neither playlists nor imported tracks
+        // They probably want this screen anyway
+        
+        if !workflow.isEmpty {
+            if isFirstLaunch {
+                workflow.addStep(
+                    .interaction(ConfirmStep.create(
+                        text: "And that's it!\nHave fun with Ten Tunes!",
+                        buttonText: "Let's Go!",
+                        mode: .complete
+                    ) { [unowned self] in
+                        self.commenceAfterWelcome()
+                    })
+                )
+            }
+            else {
+                workflow.addStep(
+                    .interaction(ConfirmStep.create(
+                        text: "Alright, that's it. Have fun!",
+                        buttonText: "Cool, thanks!",
+                        mode: .complete
+                    ) { [unowned self] in
+                        self.commenceAfterWelcome()
+                    })
+                )
+            }
+        }
+    }
+    
+    func chooseLibrary(create: Bool) -> Bool {
+        var location: URL?
+        
+        if create {
+            let dialog = NSSavePanel()
+            location = dialog.runModal() == .OK ? dialog.url : nil
+        }
+        else {
+            let dialog = NSOpenPanel()
+            
+            dialog.canChooseFiles = true // packages are considered files by finder, library is a package
+            dialog.canChooseDirectories = true
+            dialog.allowedFileTypes = ["de.ivorius.tentunes.library"]
+            dialog.directoryURL = location
+            
+            location = dialog.runModal() == .OK ? dialog.url : nil
+        }
+        
+        // TODO Allow overrides (the save dialogue specifically prompts for it)
+        return location.map { tryLibrary(at: $0, create: create) } ?? false
+    }
+
     static func switchToDJ() {
         AppDelegate.defaults[.trackWordSingular] = "track"
         AppDelegate.defaults[.trackWordPlural] = "tracks"

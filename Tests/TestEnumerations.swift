c490743c36fd654aa7677b29f8db0a4eaaf53dd3
Lukas Tenbrink <lukastenbrink@googlemail.com>
2019-05-02 12:46:13 +0200

    Add a test for Enumerations.

---
+++ Tests/TestEnumerations.swift
@@ -0,0 +1,61 @@
+//
+//  TestEnumerations.swift
+//  Tests
+//
+//  Created by Lukas Tenbrink on 02.05.19.
+//  Copyright © 2019 ivorius. All rights reserved.
+//
+
+import XCTest
+
+@testable import TenTunes
+
+class TestEnumerations: XCTestCase {
+    enum Labeled {
+        case a(aval: Any)
+        case b(bval: Any)
+    }
+    
+    enum Crossing {
+        case a(aval: Any)
+        case b(bval: Any)
+    }
+
+    enum Unlabeled {
+        case a(_ aval: Any)
+        case b(_ bval: Any)
+    }
+    
+    func testAssociatedValues() {
+        let va = "A", vb = "B"
+        
+        let a = Labeled.a(aval: va)
+        let b = Labeled.b(bval: vb)
+        
+        XCTAssertEqual(va, Enumerations.associatedValue(of: a, as: Labeled.a))
+        XCTAssertEqual(vb, Enumerations.associatedValue(of: b, as: Labeled.b))
+
+        
+        let ca = Crossing.a(aval: va)
+        let cb = Crossing.b(bval: vb)
+        
+        XCTAssertEqual(va, Enumerations.associatedValue(of: ca, as: Crossing.a))
+        XCTAssertEqual(vb, Enumerations.associatedValue(of: cb, as: Crossing.b))
+
+        
+        let ua = Unlabeled.a(va)
+        let ub = Unlabeled.b(vb)
+        
+        XCTAssertEqual(va, Enumerations.associatedValue(of: ua, as: Unlabeled.a))
+        XCTAssertEqual(vb, Enumerations.associatedValue(of: ub, as: Unlabeled.b))
+    }
+
+    func testCaseLet() {
+        let array: [Labeled] = [
+            .a(aval: "A"), .a(aval: "B"), .b(bval: "D"), .a(aval: "C")
+        ]
+        
+        let result: [String] = array.caseLet(Labeled.a)
+        XCTAssertEqual(["A", "B", "C"], result)
+    }
+}

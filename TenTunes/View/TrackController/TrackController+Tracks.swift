a9170694b9faeef2c5bb06abfb4a23fc9a3e86fe
Lukas Tenbrink <lukastenbrink@googlemail.com>
2019-05-06 22:17:04 +0200

    Fix track editor never updating when it contains a deleted track.

--- TenTunes/View/TrackController/TrackController+Tracks.swift
+++ TenTunes/View/TrackController/TrackController+Tracks.swift
@@ -340,7 +340,7 @@ extension TrackController: NSTableViewDelegate {
         let indices = Array(_tableView.selectedRowIndexes)
         let tracks = indices.map { history.track(at: $0)! }
         
-        guard !tracks.isEmpty && trackEditor.tracks.noneSatisfy({ $0.wasDeleted }) else {
+        guard !tracks.isEmpty && tracks.noneSatisfy({ $0.wasDeleted }) else {
             // Honestly, if the selection is set to void but the tracks are all still THERE
             // Then why change the view? This is especially useful if we edit a track inside a smart playlist
             
